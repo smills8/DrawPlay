025795252367eec2e5b923817190ae8bf204c287
braunyman <braun@mymail.mines.edu>
2020-02-13 13:00:00 -0700

    adjusted eye width

--- src/Cat.java
+++ src/Cat.java
@@ -7,31 +7,30 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	private static final int EYE_Y = HEAD_DIMENSION / 4;
+	private static final int EYE_X = HEAD_DIMENSION / 4;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION / 3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
+	private static final int EYE_HEIGHT = 21;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION / 4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
-	
+	private static final int MOUTH_X = HEAD_DIMENSION / 5 * 2;
+	private static final int MOUTH_Y = HEAD_DIMENSION / 5 * 3;
+
 	// draw will render the Cat on the Graphics object
-	public void draw(Graphics g, int catX, int catY)
-	{
+	public void draw(Graphics g, int catX, int catY) {
 		Graphics2D g2 = (Graphics2D) g;
-		int x=catX;
-		int y=catY;
+		int x = catX;
+		int y = catY;
 		// Draw the head
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.green);
-		x = catX + EYE_X; 
+		x = catX + EYE_X;
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
@@ -41,9 +40,9 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
+		g2.setColor(Color.red);
+		// Meow text appears below cat head, +10 places below
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow", catX, catY + HEAD_DIMENSION + 10);
 	}
 }
